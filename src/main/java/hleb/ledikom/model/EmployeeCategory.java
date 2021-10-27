7f72dbf8558f788b6a87b9ff98dbe0f90736aa40
hlebs <glebszypula1997@gmail.com>
2021-10-27 19:29:45 +0200

    employee, notificationTerm add

--- src/main/java/hleb/ledikom/model/EmployeeCategory.java
+++ src/main/java/hleb/ledikom/model/EmployeeCategory.java
@@ -1,14 +1,3 @@
 package hleb.ledikom.model;
 
-//public enum EmployeeCategory {
-//
-//    HIGHEST("Wyższa"),
-//    FIRST("Pierwsza"),
-//    SECOND("Druga");
-//
-//    public final String label;
-//
-////    EmployeeCategory(String label) {
-////        this.label = label;
-////    }
-//}
+
